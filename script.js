cec961ed4bb7898091727f57d5d4810ed2a9c08a
kylian-2612 <71694721+kylian-2612@users.noreply.github.com>
2021-12-14 12:05:14 +0100

    basic scriptfile from api

---
+++ script.js
@@ -0,0 +1,22 @@
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=denderleeuw&appid=d3cdfff8a9c9b9f89b250cc4b6bc8e18';
+
+
+
+
+async function getData(url){
+
+    let response = await fetch(url);
+
+    return await response.json();
+
+}
+
+
+
+getData(url).then(movie => {
+
+    console.log('movie');
+
+    document.getElementById('message').innerText = JSON.stringify(movie);
+
+});
